a51c35f6d0b141018dd4e7867f491be077a71cc5
unknown <437172242@qq.com>
2026-03-02 16:45:59 +0800

    a

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,14 +1,30 @@
-
+import torch
 import numpy as np
 import sys
-sys.path.append(r'D:\pycode\SimCLR-master\models')
+sys.path.append(r'D:\code\SimCLR-master\models')
 from resnet_simclr import ResNetSimCLR
-from torchvision.transforms import transforms
+# from torchvision.transforms import transforms
 import torchvision.transforms.v2 as transforms
 import torch
 from gaussian_blur import GaussianBlur
 # See https://keras.io/api/applications/ for details
+PATCH_SIZE = 16
+IMAGE_SIZE = 768
 
+# quantization filter for the given patch size
+patch_quant_filter = torch.nn.Conv2d(1, 1, PATCH_SIZE, stride=PATCH_SIZE, bias=False)
+patch_quant_filter.weight.data.fill_(1.0 / (PATCH_SIZE * PATCH_SIZE))
+
+# image resize transform to dimensions divisible by patch size
+def resize_transform(
+    mask_image,
+    image_size: int = IMAGE_SIZE,
+    patch_size: int = PATCH_SIZE,
+) -> torch.Tensor:
+    w, h = mask_image.size
+    h_patches = int(image_size / patch_size)
+    w_patches = int((w * image_size) / (h * patch_size))
+    return TF.to_tensor(TF.resize(mask_image, (h_patches * patch_size, w_patches * patch_size)))
 class FeatureExtractor1:
     def __init__(self):
         base_model = VGG16(weights='imagenet')
@@ -23,31 +39,45 @@ class FeatureExtractor1:
         Returns:
             feature (np.ndarray): deep feature with the shape=(4096, )
         """
-        img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
-        img = img.convert('RGB')  # Make sure img is color
-        x = image.img_to_array(img)  # To np.array. Height x Width x Channel. dtype=float32
-        x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C), where the first elem is the number of img
-        x = preprocess_input(x)  # Subtracting avg values for each pixel
-        feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
-        return feature / np.linalg.norm(feature)  # Normalize
+        patch_mask_values = []
+        patch_features = []
+        with torch.inference_mode():
+            with torch.autocast(device_type='cuda', dtype=torch.float32):
+                for image, mask in tqdm([(image_left, mask_left), (image_right, mask_right)], desc="Processing images"):
+                    # processing mask
+                    mask = mask.split()[-1]
+                    mask_resized = resize_transform(mask)
+                    #mask_quantized = patch_quant_filter(mask_resized).squeeze().view(-1).detach().cpu()
+                    mask_quantized = patch_quant_filter(mask_resized).squeeze().detach().cpu()
+                    patch_mask_values.append(mask_quantized)
+                    # processing image
+                    image = image.convert('RGB')
+                    image_resized = resize_transform(image)
+                    image_resized = TF.normalize(image_resized, mean=IMAGENET_MEAN, std=IMAGENET_STD)
+                    image_resized = image_resized.unsqueeze(0).cuda()
+
+                    feats = model.get_intermediate_layers(image_resized, n=range(n_layers), reshape=True, norm=True)
+                    dim = feats[-1].shape[1]
+                    #patch_features.append(feats[-1].squeeze().view(dim, -1).permute(1,0).detach().cpu())
+                    patch_features.append(feats[-1].squeeze().detach().cpu())
 
 
 
 class FeatureExtractor2:
-    def __init__(self,path):
+    def __init__(self,path,device):
 ##        base_model = VGG16(weights='imagenet')
 ##        self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
         s=1
         color_jitter =transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
         size=96
-##        self.transforms=transforms.Compose([transforms.Resize(size=size),
+##        self.transform=transforms.Compose([transforms.Resize(size=size),
 ####                                              transforms.RandomResizedCrop(size=size),
 ####                                              transforms.RandomHorizontalFlip(),
 ####                                              transforms.RandomApply([color_jitter], p=0.8),
 ####                                              transforms.RandomGrayscale(p=0.2),
 ##                                              GaussianBlur(kernel_size=int(0.1 * size)),
 ##                                              transforms.ToTensor()])
-##        self.transforms = transforms.Compose([
+##        self.transform = transforms.Compose([
 ####                                            transforms.RandomRotation(180),
 ####                                            transforms.RandomResizedCrop(scale=(0.8,1.5),ratio=(0.8,1.25),size=size),
 ##                                            
@@ -57,24 +87,35 @@ class FeatureExtractor2:
 ####                                              GaussianBlur(kernel_size=int(0.1 * size)),
 ##                                              transforms.Resize(size),
 ##                                              transforms.ToTensor()])
-        self.transforms=transforms.Compose([
+        self.transform=transforms.Compose([
                                     transforms.Resize(size=(size,size)),
                                     # transforms.RandomResizedCrop(scale=(0.8,1.5),ratio=(0.8,1.25),size=size),
                                     transforms.Compose([transforms.ToImage(), transforms.ToDtype(torch.float32, scale=True)]),
                                     ])
         self.model=ResNetSimCLR('resnet18',128)
         self.model.load_state_dict(torch.load(path)['state_dict'])
+        self.device=device
+        self.model=self.model.to(self.device)
         self.model.eval()
 
     def extract(self, img):
-##        print(type(img))
-        input=self.transforms(img.convert('RGB'))
-##        if input.shape==1:
-##
-        with torch.no_grad():
-            output=torch.nn.functional.normalize(self.model(input.unsqueeze(0)),dim=1)[0]
-        return output.detach().numpy()
+#         input=self.transform(img.convert('RGB'))
+# ##        if input.shape==1:
+# ##
+#         with torch.no_grad():
+#             output=torch.nn.functional.normalize(self.model(input.unsqueeze(0)),dim=1)[0]
+#         return output.detach().numpy()
 
+        input = self.transform(img).unsqueeze(0).to(self.device)
+    # with torch.autocast('cuda', dtype=torch.bfloat16):
+    #     batch_img = transform(img)[None]
+    #     batch_img = batch_img
+    #     depths = depther(batch_img)
+        with torch.no_grad():
+            # output = torch.nn.functional.normalize(self.model(input), dim=1)[0]
+            # output = torch.nn.functional.normalize(self.model(input), dim=1)[0]
+            output = self.model(input)[0]
+        return output
 
 ##class FeatureExtractor3:
 ##    def __init__(self,device):
@@ -142,3 +183,53 @@ class FeatureExtractor2:
 ##        image = torch.Tensor(image).unsqueeze_(dim=0)  # (b,h,w,c)
 ##        image = image.permute((0, 3, 1, 2)).float()  # (b,h,w,c) -> (b,c,h,w)
 ##        return (self.model(image.to(self.device)).detach()-self.a)/self.b
+
+
+def make_transform(resize_size: int | list[int] = 768):
+    to_tensor = transforms.ToImage()
+    resize = transforms.Resize((resize_size, resize_size), antialias=True)
+    to_float = transforms.ToDtype(torch.float32, scale=True)
+    normalize = transforms.Normalize(
+        mean=(0.485, 0.456, 0.406),
+        std=(0.229, 0.224, 0.225),
+    )
+    return transforms.Compose([to_tensor, resize, to_float, normalize])
+class FeatureExtractor4:
+    def __init__(self,device):
+        # self.model=dinov3_convnext_large = torch.hub.load("facebookresearch/dinov3", 'dinov3_convnext_large', source='github', weights='dinov3_convnext_large_pretrain_lvd1689m-61fa432d.pth').to(device)
+        self.device=device  
+        self.load_model()
+        img_size = 224
+        self.transform=make_transform(img_size)
+        self.model.eval()
+
+    def load_model(self,):
+        self.model=dinov3_convnext_large = torch.hub.load(r"C:\Users\Administrator\.cache\torch\hub\facebookresearch_dinov3_main", 'dinov3_convnext_large', source='local', weights='dinov3_convnext_large_pretrain_lvd1689m-61fa432d.pth').to(self.device)
+
+    
+    def extract(self, img):
+        input = self.transform(img).unsqueeze(0).to(self.device)
+    # with torch.autocast('cuda', dtype=torch.bfloat16):
+    #     batch_img = transform(img)[None]
+    #     batch_img = batch_img
+    #     depths = depther(batch_img)
+        with torch.no_grad():
+            # output = torch.nn.functional.normalize(self.model(input), dim=1)[0]
+            # output = torch.nn.functional.normalize(self.model(input), dim=1)[0]
+            output = self.model(input)[0]
+        return output
+    
+class FeatureExtractor5(FeatureExtractor4):
+    def load_model(self,):
+        # self.model= torch.hub.load("facebookresearch/dinov2", 'dinov2_vitl14', source='github', weights='dinov2_vitl14_pretrain.pth').to(self.device)
+        self.model= torch.hub.load(r"C:\Users\Administrator\.cache\torch\hub\facebookresearch_dinov2_main", 'dinov2_vitl14', source='local', weights='dinov2_vitl14_pretrain.pth').to(self.device)
+    
+class FeatureExtractor6(FeatureExtractor4):
+    def load_model(self,):
+        self.model= torch.hub.load(r"C:\Users\Administrator\.cache\torch\hub\facebookresearch_dinov3_main", 'dinov3_vith16plus', source='local', weights='dinov3_vith16plus_pretrain_lvd1689m-7c1da9a5.pth').to(self.device)
+if __name__ == '__main__':
+    fe1=FeatureExtractor4(device='cpu')
+    
+    # print(fe1.model)
+    print(fe1.extract(np.zeros((224,224,3),dtype=np.uint8)).shape)
+    # print(fe1.extract(torch.zeros((224,224,3),dtype=torch.uint8)).shape)
--- server2.py
+++ server2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
-from feature_extractor import FeatureExtractor1,FeatureExtractor2
+import torch
+from feature_extractor import FeatureExtractor1,FeatureExtractor2,FeatureExtractor5, FeatureExtractor6
 from datetime import datetime
 import time
 from flask import Flask, request, render_template,jsonify
@@ -8,6 +9,8 @@ from pathlib import Path
 import argparse
 import shutil,os,json,requests,cv2
 import logging
+import io
+import base64
 
 logging.basicConfig(level=logging.INFO)
 
@@ -48,31 +51,35 @@ def convert_image_path2(dst):
 
 
 
-def get_score(img):
-    query = fe.extract(img)
-####        dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
-##    print(query.shape,features.shape)
-##    dists=-np.mean(features*query,axis=1)/np.sqrt((np.mean(features**2,axis=1)*np.mean(query**2)))
-##    print(dists.shape)
-##    ids = np.argsort(dists)[:30]  # Top 30 results
-    sim=features@query
-##    app.logger.info(str((sim.shape,sim.max(),sim.min(),sim.mean())))
-    ids = np.argsort(-sim)[:30]
+# def get_score(img):
+#     query = fe.extract(img)
+# ####        dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
+# ##    print(query.shape,features.shape)
+# ##    dists=-np.mean(features*query,axis=1)/np.sqrt((np.mean(features**2,axis=1)*np.mean(query**2)))
+# ##    print(dists.shape)
+# ##    ids = np.argsort(dists)[:30]  # Top 30 results
+#     sim=features@query
+# ##    app.logger.info(str((sim.shape,sim.max(),sim.min(),sim.mean())))
+#     ids = np.argsort(-sim)[:30]
     
-    scores = [(sim[id], '/'.join(['static','img2',l2[id][0]+'_'+l2[id][1]+'.jpg']),l2[id][2]) for id in ids]
-    return scores
+#     scores = [(sim[id], '/'.join(['static','img2',l2[id][0]+'_'+l2[id][1]+'.jpg']),l2[id][2]) for id in ids]
+#     return scores
+def get_score(img):
+    return new_model.search(img)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     print('a')
     if request.method == 'POST':
-        file = request.files['query_img']
-
-        # Save query image
-        img = Image.open(file.stream).convert('RGB')  # PIL image
-        uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
-        img.save(uploaded_img_path)
-
+        # file = request.files['query_img']
+        filepath=request.form.get('filepath',None)
+        filename=filepath.split('/')[-1].split('\\')[-1]
+        img=Image.open(filepath).convert('RGB')
+        # # Save query image
+        # img = Image.open(file.stream).convert('RGB')  # PIL image
+        uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + filename
+        # img.save(uploaded_img_path)
+        shutil.copy2(filepath,uploaded_img_path)
         scores=get_score(img)
 
         return render_template('index1.html',
@@ -91,32 +98,98 @@ def index2():
     else:
         filename=request.args.get('file',None)
         keywords=request.args.get('keywords',None)
+        assert keywords,[filename,keywords]
         if filename:
-            query = fe.extract(Image.open(filename))
-            assert keywords,[filename,keywords]
-            keywords=keywords.lower()
-            scores=[]
-            for j,i in enumerate(l2):
-                if keywords in i[3].lower():
-                    scores.append([features[j]@query,f'static/img2/{i[0]}_{i[1]}.jpg',i[2],i[3]])
-            scores.sort(key=lambda x:-x[0])
-            uploaded_img_path=filename
+            dis=[i.item() for i in new_model.cal(Image.open(filename))]
         else:
-            img_req = requests.get('http://192.168.101.17:8080//' + 'shot.jpg')
-            img_arr = np.array(bytearray(img_req.content), dtype=np.uint8)
-            img = cv2.imdecode(img_arr, -1)
-            img=cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-            img =Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
-            uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + 'a.jpg'
-            img.save(uploaded_img_path)
+            dis=[0 for _ in range(len(new_model.filenames))]
+        keywords=keywords.lower()
+        scores=[]
+        uploaded_img_path=filename
+        for j,d in enumerate(merged_data):
+            title=d['title']
+            if keywords not in title.lower():
+                continue
+            print(title,[d['surugaya_code'],d['doujinshiorg_code']])
+            s=''
+            if d['surugaya_code'] and d['surugaya_code'] in bought_items:
+                s=f'{bought_items[d["surugaya_code"]]} '
+            s=f'{s} {d["surugaya_code"] and d["surugaya_code"] in bought_items2} {d["surugaya_code"] and d["surugaya_code"] in bought_items2} {bought_filenames.get(d["surugaya_code"],[])} {bought_filenames.get(d["doujinshiorg_code"],[])}  {d['surugaya_code']} {d["doujinshiorg_code"]} {j}'
+            for i in [d['surugaya_code'],d['doujinshiorg_code']]:
+                if i:
+                    if i in surugaya_data:
+                        s=f'{surugaya_data[i]["date_price"].get(surugaya_sorted_date[-1], None)} {s} '
+                    for id_,filename in enumerate(new_model.filenames):
+                        a=filename.split('/')
+                        b=a[-3]
+                        assert b in ['surugaya','doujinshiorg'],f'{filename} {a}'
+                        c=a[-1].split('_')[0].split('.')[0]
+                        if c.lower()==i.lower():
+                            scores.append([dis[id_].item(), '/'.join(['static','img2',b+'_'+c+'.jpg']),title,s])
+                            break
+                    else:
+                        print((i,keywords,d))
+                        scores.append([dis[id_].item(), None,title,s])
+        print(len(scores))
+        scores.sort(key=lambda x:-x[0])
+        # else:
+        #     img_req = requests.get('http://192.168.101.17:8080//' + 'shot.jpg')
+        #     img_arr = np.array(bytearray(img_req.content), dtype=np.uint8)
+        #     img = cv2.imdecode(img_arr, -1)
+        #     img=cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+        #     img =Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+        #     uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + 'a.jpg'
+        #     img.save(uploaded_img_path)
 
 
-            scores=get_score(img)
+        #     scores=get_score(img)
 
         return render_template('index1.html',
                                query_path=uploaded_img_path,
                                scores=scores)
 
+
+@app.route('/region', methods=['GET'])
+def region_index():
+    return render_template('index_region.html')
+
+@app.route('/region/upload', methods=['POST'])
+def region_upload():
+    file = request.files['image']
+    img = Image.open(file.stream).convert('RGB')
+    uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
+    img.save(uploaded_img_path)
+    return jsonify({'path': uploaded_img_path})
+
+@app.route('/region/search', methods=['POST'])
+def region_search():
+    data = request.get_json()
+    image_path = data['image_path']
+    regions = data['regions']
+    
+    img = Image.open(image_path).convert('RGB')
+    results = []
+    
+    for i, region in enumerate(regions):
+        x0, y0, x1, y1 = region['x0'], region['y0'], region['x1'], region['y1']
+        # 裁剪区域
+        cropped_img = img.crop((x0, y0, x1, y1))
+        
+        # 将裁剪后的图片转为base64
+        buffered = io.BytesIO()
+        cropped_img.save(buffered, format="JPEG")
+        cropped_img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        
+        # 获取该区域的检索分数
+        scores = get_score(cropped_img)
+        
+        results.append({
+            'region': region,
+            'cropped_image': f'data:image/jpeg;base64,{cropped_img_base64}',
+            'scores': [[float(s[0]), *s[1:]] for s in scores[:20]]  # 取前20个结果
+        })
+    
+    return jsonify({'results': results})
 
 @app.route('/a', methods=[ 'GET','POST'])
 def index3():
@@ -139,7 +212,7 @@ def index3():
             query = fe.extract(Image.open('static/uploaded/'+filename))
             for j,i in enumerate(l2):
                 if keywords in i[3].lower():
-                    scores.append([features[j]@query,f'static/img2/{i[0]}_{i[1]}.jpg',i[2],i[3]])
+                    scores.append([features[j]@query,f'static/img2/{i[0]}_{i[1]}.jpg',i[2],i[3],s])
             scores.sort(key=lambda x:-x[0])
             
         else:
@@ -150,49 +223,116 @@ def index3():
                                query_path='static/uploaded/'+filename,
                                scores=scores,
                                next_file=next_file)
+    
+class model:
+    def __init__(self,device='cpu'):
+        self.name_extractor2={'dinov2_vitl14_pretrain':FeatureExtractor5(device=device),'dinov3_vith16plus':FeatureExtractor6(device=device),'resnet18_simclr_checkpoint_0100':FeatureExtractor2('checkpoint_0100.pth.tar',device=device)}
+
+        data=torch.load('features.pt')
+
+        self.filenames=data['filenames']
+        self.func=lambda x:(x-x.mean(dim=1,keepdim=True))/(x.std(dim=1,keepdim=True)+1e-8)
+        self.func3=lambda a,b:-torch.cosine_similarity(a, b, dim=-1)
+        self.name_features={k:self.func(v[:len(self.filenames)])[:,None] for k,v in data['name_features'].items() if k in self.name_extractor2}
+
+        self.data_dir=r'D:\marimite\doujin\doujinshidata'
+
+
+    def cal(self,img):
+        dis=0
+        for name,fe in self.name_extractor2.items():
+            dis+=self.func3(self.name_features[name], self.func(fe.extract(img)[None])[None])
+        # print(self.name_features[name][:5,:5],self.func(fe.extract(img)[None])[:5,:5])
+        return dis
+    
+    def search(self,img,num=30):
+        dis=self.cal(img)
+        ids = torch.argsort(dis,dim=0,descending=False)[:num,0]
+        print(ids.shape,dis.shape,ids)
+        scores=[]
+        for id in ids:
+            filename=self.filenames[id]
+            a=filename.split('/')
+            b=a[-3]
+            assert b in ['surugaya','doujinshiorg'],f'{filename} {a}'
+            c=a[-1].split('_')[0].split('.')[0]
+            for i,d in enumerate(merged_data):
+                if (d['surugaya_code'] and d['surugaya_code'].lower()==c) or (d['doujinshiorg_code'] and d['doujinshiorg_code'].lower()==c):
+                    title=d['title']
+                    s=''
+                    if d['surugaya_code']:
+                        s=f'{surugaya_data[d["surugaya_code"]]["date_price"].get(surugaya_sorted_date[-1], None)} {bought_items.get(d["surugaya_code"],None)} '
+                    s=f'{s} {d["surugaya_code"] and d["surugaya_code"] in bought_items2} {bought_filenames.get(d["surugaya_code"],[])} {bought_filenames.get(d["doujinshiorg_code"],[])} {d['surugaya_code']} {d["doujinshiorg_code"]} {i}'
+                    scores.append([dis[id].item(), '/'.join(['static','img2',b+'_'+c+'.jpg']),title,s])
+                    break
+            else:
+                raise ValueError(f'{c} {filename} not found in merged_data')
+        return scores
+        
+new_model=model('cpu')
 if __name__=="__main__":
     data_dir=r'D:\marimite\doujin\doujinshidata'
     with open(os.path.join(data_dir,'data_merged.json'),'r',encoding='utf-8') as f:
         merged_data=json.load(f)
     print(len(merged_data))
-    fe=FeatureExtractor2('checkpoint_0100.pth.tar')
-    l1=[]
-    l2=[]
-    features = []
-    for i,a in enumerate(merged_data):
-        doujinshiorg_code=a['doujinshiorg_code']
-        surugaya_code=a['surugaya_code']
-        if surugaya_code:
-            l1.append(['surugaya',surugaya_code,i,a['title']])
-        if doujinshiorg_code:
-            l1.append(['doujinshiorg',doujinshiorg_code,i,a['title']])
+    with open(r'D:\chrome_download\surugaya_all_bought.json','r',encoding='utf-8') as f:
+        bought_data=json.load(f)
+    bought_items=bought_data['bought_items']
+    bought_items2=set(bought_data['bought_items2'])
+    bought_items3=bought_items2.union(bought_items)
+    
+    bought_filenames={}
+    for i in os.listdir('static/result/'):
+        a=i.split('_')[1]
+        if a not in bought_filenames:
+            bought_filenames[a]=[]
+        bought_filenames[a].append(i)
 
-    feature_path=os.path.join('static','feature')
-    if not os.path.exists(feature_path):
-        os.mkdir(feature_path)
-    for i in l1:
-        path=os.path.join(data_dir,i[0],'picture',i[1]+('_0' if i[0]=='doujinshiorg' else '')+'.jpg')
-        if not os.path.exists(path):
-            print(path)
-            continue
-        l2.append(i)
-        path2=os.path.join('static','img2',i[0]+'_'+i[1]+'.jpg')
-        if not os.path.exists(path2):
-            shutil.copy(path,path2)
-        path3=os.path.join(feature_path,i[0]+'_'+i[1]+'.npy')
+    with open(r'd:\marimite\doujin\doujinshidata\surugaya\data.json','r',encoding='utf-8') as f:
+        surugaya_data=json.load(f)
+    surugaya_date_set=set()
+    for i in surugaya_data.values():
+        surugaya_date_set.update(i['date_price'])
 
-        if os.path.exists(path3):
-            feature=np.load(path3)
-        else:
-            feature=fe.extract(img=Image.open(path2))
-            np.save(path3, feature)
+    surugaya_sorted_date=sorted(surugaya_date_set)
+#     fe=FeatureExtractor2('checkpoint_0100.pth.tar','cpu')
+#     l1=[]
+#     l2=[]
+#     features = []
+#     for i,a in enumerate(merged_data):
+#         doujinshiorg_code=a['doujinshiorg_code']
+#         surugaya_code=a['surugaya_code']
+#         if surugaya_code:
+#             l1.append(['surugaya',surugaya_code,i,a['title']])
+#         if doujinshiorg_code:
+#             l1.append(['doujinshiorg',doujinshiorg_code,i,a['title']])
+
+#     feature_path=os.path.join('static','feature')
+#     if not os.path.exists(feature_path):
+#         os.mkdir(feature_path)
+#     for i in l1:
+#         path=os.path.join(data_dir,i[0],'picture',i[1]+('_0' if i[0]=='doujinshiorg' else '')+'.jpg')
+#         if not os.path.exists(path):
+#             print(path)
+#             continue
+#         l2.append(i)
+#         path2=os.path.join('static','img2',i[0]+'_'+i[1]+'.jpg')
+#         if not os.path.exists(path2):
+#             shutil.copy(path,path2)
+#         path3=os.path.join(feature_path,i[0]+'_'+i[1]+'.npy')
+
+#         if os.path.exists(path3):
+#             feature=np.load(path3)
+#         else:
+#             feature=fe.extract(img=Image.open(path2))
+#             np.save(path3, feature)
         
-##        feature=fe.extract(img=Image.open(path2))
+# ##        feature=fe.extract(img=Image.open(path2))
         
-        features.append(feature)
-    print(len(l1),len(l2))
-    features = np.array(features)
-    print(features.shape)
-    a=features@features.T
-    print(a.shape,a.max(),a.min(),a.mean())
+#         features.append(feature)
+#     print(len(l1),len(l2))
+#     features = np.array(features)
+#     print(features.shape)
+#     a=features@features.T
+#     print(a.shape,a.max(),a.min(),a.mean())
     app.run(port=5002,debug=True)
